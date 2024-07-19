16494c778027f74baf94c9ee4d9a2c35acedc3b9
YenCheng Lai <yencheng.lai@zyxel.com.tw>
2024-07-19 14:27:10 +0800

    add gen_mongo_check_cmd function and some minor adjustments

--- tasks/generate_api_keys.py
+++ tasks/generate_api_keys.py
@@ -2,17 +2,40 @@ import random
 import string
 import sys
 
+
 class APIKeyGenerator:
 
+    key: str | None = None
+
     def __init__(self, key_length=16):
+        if key_length < 1:
+            raise ValueError("Key length must be at least 1")
         self.key_length = key_length
 
-    def gen_api_key(self) -> str:
+    def gen_api_key(self) -> None:
+        """
+        Generates random api keys with specified length
+
+        Returns:
+            generated API key
+        """
         random_key = "".join(
             random.choice(string.ascii_letters + string.digits)
             for _ in range(self.key_length)
         )
-        return "AU" + random_key
+        self.key = "AU" + random_key
+
+    def gen_mongo_check_cmd(self) -> str:
+        """
+        Generates a MongoDB find command to check for the provided API key.
+
+        Args:
+            api_key: The API key to check for.
+
+        Returns:
+            A string representing the MongoDB find command.
+        """
+        return f"db.account.find({{ api_key: /^{self.key}/ }})"
 
 
 def main():
@@ -21,12 +44,15 @@ def main():
         try:
             key_length = int(sys.argv[1])
             generator = APIKeyGenerator(key_length)
-        except ValueError:
-            print("Invalid argument. Please provide an integer for key length.")
+        except ValueError as e:
+            print(e.args[0])
             exit(1)
     else:
         generator = APIKeyGenerator()
-    print(f"Generated API keys: {generator.gen_api_key()}")
+    generator.gen_api_key()
+    print(f"Generated API keys: {generator.key}")
+    print("Please run this command in MongoDB to check if duplicate exists:")
+    print(generator.gen_mongo_check_cmd())
 
 
 if __name__ == "__main__":
